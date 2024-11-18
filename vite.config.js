bb0e8db902b5a4fbdd8cb3f8dc27837324a75f34
Eric Johnson <eric3517@icloud.com>
2024-11-18 09:19:21 -0600

    post-build copy to docs for github pages to pick up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // The base is relative, because this app lives at /lostbearlabs.com/guitar-quiz
   base: './',
   plugins: [react()],
 })
